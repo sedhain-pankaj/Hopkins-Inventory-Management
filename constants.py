3b21cedb3b9f4d503584d1e74e8cc162dfb3bc3e
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2024-07-06 22:45:37 +1000

    added overall stock csv path

--- constants.py
+++ constants.py
@@ -6,6 +6,8 @@ FONT_SIZE = 70 if os.name == "nt" else 128
 # Time delay in milliseconds for updating the time label
 TIME_DELAY = 1000
 
-# File path for the cornice rate csv file
+# File paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FILEPATH = os.path.join(BASE_DIR, "assets", "cornice_rate.csv")
+CORNICE_RATE_FILEPATH = os.path.join(BASE_DIR, "assets", "cornice_rate.csv")
+OVERALL_STOCK_FILEPATH = os.path.join(BASE_DIR, "assets", "overall_stock.csv")
+
